566a2f8949c617619243cadc6e7ef4b1b090541c
trissaik <trissaikyt.g@gmail.com>
2023-12-21 12:39:57 +0100

    init

---
+++ lua/noob-vim/init.lua
@@ -0,0 +1,31 @@
+local M = {}
+local string = require("string")
+
+local map = {
+	c = "//",
+	lua = "--",
+	java = "//",
+}
+
+function string.starts(String, Start)
+	return string.sub(String, 1, string.len(Start)) == Start
+end
+function M.setup(opts)
+	opts = opts or {}
+	vim.keymap.set("n", "<Leader>c/", function()
+		local line = vim.api.nvim_get_current_line()
+		local trimmed = line:match("^%s*(.-)%s*$")
+		local comment = map[vim.bo.filetype]
+		if string.starts(trimmed, comment) then
+			line = string.gsub(line, "%-%-", "")
+			vim.api.nvim_set_current_line(line)
+			print("1 " .. line)
+		else
+			line = comment .. line
+			vim.api.nvim_set_current_line(line)
+			print("2 " .. line)
+		end
+	end, { noremap = true, silent = true, desc = "Comment line" })
+end
+
+return M
